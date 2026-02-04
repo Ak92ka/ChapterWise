fc7bd2fa750e89f26e79099cbbb22721e2d55024
Ahmed <ahmedalkhalaf@outlook.fr>
2026-02-04 16:51:12 +0100

    frontend url

--- chapteriq-backend/app.js
+++ chapteriq-backend/app.js
@@ -120,7 +120,7 @@ app.post(
 
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000", // frontend origin
+  origin: process.env.FRONTEND_URL || "https://chapteriq.vercel.app", // frontend origin
   credentials: true, // <-- allows cookies to be sent
 }));
 app.use(express.json({ limit: "5mb" }));
